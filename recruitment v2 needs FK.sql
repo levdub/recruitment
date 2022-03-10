7cb1fd1e8def2b206695d4a640a5bdcfaadf7bdb
CHatten1527 <101286550+CHatten1527@users.noreply.github.com>
2022-03-09 18:01:51 -0600

    Add files via upload
    This is updated to represent what we have talked about in class and I can't figure out how to setup foreign keys they need to be setup in the communication table to connect to the other three tables. 
    
    Look in Docs at the phase 2 ERD as a basic blueprint remember that Mrs. Angela told us to remove the connection between the student and activities table which is why it is not there.
    I also removed state, city, and zip from students. I added a "local" colomn that is tinyint (boolean) that we can use to track if students are in or out of the country. I also added a "ststus" which is if they should continue to reach out to this student or not. And finally an "activitiesCounter" which counts down from 30 and will reset once a communication has been made with the student or if they are deactivated it will stay on 30 permanently. (This stuff we need to code. Im just explaining how it should work.
    
    In the activity table I added a name section and made the description and the date nullable so that they could add basic activities like "sent email", "met in office" things they may not want to date.
    
    In the users table I gave them "email" (so we can send emails to them) and a "privledged rights" colomn so we can allow only certain teachers to have more control over the applicationthan others for deletiong stuff later for example.

---
+++ recruitment v2 needs FK.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 10, 2022 at 01:00 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `recruitment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activities`
+--
+
+CREATE TABLE `activities` (
+  `activityID` int(11) NOT NULL,
+  `activityDate` date DEFAULT NULL,
+  `activityDesc` varchar(200) DEFAULT NULL,
+  `activityName` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `activities`
+--
+
+INSERT INTO `activities` (`activityID`, `activityDate`, `activityDesc`, `activityName`) VALUES
+(1, '2022-02-01', 'USA Day.', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `communications`
+--
+
+CREATE TABLE `communications` (
+  `commID` int(11) NOT NULL,
+  `studentID` int(11) NOT NULL,
+  `UsersID` int(11) NOT NULL,
+  `ActivityID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `communications`
+--
+
+INSERT INTO `communications` (`commID`, `studentID`, `UsersID`, `ActivityID`) VALUES
+(1, 20220201, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `studentID` int(11) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `jagNum` varchar(9) DEFAULT NULL,
+  `birthdate` date NOT NULL,
+  `phoneNum` varchar(10) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `local` tinyint(1) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `term` varchar(20) NOT NULL,
+  `activitiesCounter` int(11) NOT NULL DEFAULT 30
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`studentID`, `firstName`, `lastName`, `jagNum`, `birthdate`, `phoneNum`, `email`, `address`, `local`, `status`, `term`, `activitiesCounter`) VALUES
+(1, 'Test', 'Student1', NULL, '2022-02-01', '5551110000', 'test@student.com', 'Test Student Dr.', 0, 0, '', 30);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userID` int(4) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `privledgedRights` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `username`, `password`, `email`, `privledgedRights`) VALUES
+(1, 'admin', 'pa55word', '', 0),
+(2, 'user', 'pa55word', '', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activities`
+--
+ALTER TABLE `activities`
+  ADD PRIMARY KEY (`activityID`);
+
+--
+-- Indexes for table `communications`
+--
+ALTER TABLE `communications`
+  ADD PRIMARY KEY (`commID`),
+  ADD KEY `studentID` (`studentID`),
+  ADD KEY `UsersID` (`UsersID`),
+  ADD KEY `ActivityID` (`ActivityID`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`studentID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activities`
+--
+ALTER TABLE `activities`
+  MODIFY `activityID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `communications`
+--
+ALTER TABLE `communications`
+  MODIFY `commID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `studentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
