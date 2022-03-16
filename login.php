454455804629f94bb571c770dbb6923f7a2ab5d6
levdub <100220944+levdub@users.noreply.github.com>
2022-03-16 11:12:56 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,14 @@
+<!--this is a comment in HTML-->
+/*this is a comment in php*/
+
+<html>
+  
+  <h1> Administrator Login </h1>
+  <form action="." method="post" id="login">
+  <label> Username: </label>
+  <input type="text" name="username">
+  <label> Password: </label>
+  <input type="text" name="password">
+  <input type="submit" value="Login">
+  
+</html>
