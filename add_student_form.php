09a212c0591695b4dd0dae808535c05b51423c85
levdub <100220944+levdub@users.noreply.github.com>
2022-03-16 11:49:15 -0500

    Create add_student_form.php

---
+++ add_student_form.php
@@ -0,0 +1,11 @@
+<!--form for adding student, mainly HTML, processing in PHP done on add_student.php-->
+<html>
+  <head>
+  <title>Recruitment Database</title>
+  <link rel="styleesheet" type="text/css" href="main.css">
+  </head>
+  <body>
+    <main>
+      <h1>Add Student</h1>
+      
+  
