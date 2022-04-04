c87e2651aa5eb7f4a28784cd7655542bae080d7d
Kimita-san <101320047+Kimita-san@users.noreply.github.com>
2022-04-04 09:43:00 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,31 @@
-<!--this is a comment in HTML-->
-/*this is a comment in php*/
+<?php
+//this is a comment in php
+require 'database.php';
+if(!empty($_SESSION["id"])){
+  header("Location: index.php");
+}
+ if(isset($_POST["submit"])){
+   $usernameemail = $_POST["username"];
+   $password = $_POST["password"];
+   $result = mysqli_query($conn, "SELECT * FROM recruitment WHERE username = '$usernameemail' OR email = '$usernameemail'");
+   $row = mysqli_fetch_assoc($result);
+   if (mysqli_num_rows($result) > 0){
+     if ($password == $row['password']){
+       $_SESSION["login"] = true;
+       $_SESSION["id"] = $row["id"];
+       header("Location: index.php");
+   }
+   else{
+     echo
+     "<script> alert('Wrong Password'); </script>";
+   }
+ }
+ else{
+   echo
+   "<script> alert('user Not Registered'); </script>";
+ }
+}
+ ?>
 
 <html>
-  
-  <h1> Administrator Login </h1>
-  <form action="." method="post" id="login">
-  <label> Username: </label>
-  <input type="text" name="username">
-  <label> Password: </label>
-  <input type="text" name="password">
-  <input type="submit" value="Login">
-  
-</html>
+<head>
